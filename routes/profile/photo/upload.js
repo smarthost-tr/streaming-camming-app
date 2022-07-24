d239be3d018f5ceb635d360c8e3eb37fdc93f51d
halid digem <info@smart-host.com.tr>
2022-07-24 07:21:25 +0300

    ..

--- routes/profile/photo/upload.js
+++ routes/profile/photo/upload.js
@@ -2,48 +2,48 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const app = express();
+// const app = express();
 const config = require("config");
-const mongo = require("mongodb");
-const AWS = require('aws-sdk');
-const uuid = require('uuid/v4');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+// const mongo = require("mongodb");
+// const AWS = require('aws-sdk');
+// const uuid = require('uuid/v4');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
 const Grid = require("gridfs-stream");
-const GridFsStorage = require('multer-gridfs-storage');
-const axios = require("axios");
+// const GridFsStorage = require('multer-gridfs-storage');
+// const axios = require("axios");
 
 Grid.mongo = mongoose.mongo;
 
-AWS.config.update({
-    secretAccessKey: 'r8qpagbd990S45DZUU5wVFTW7K5jG75/NUc0ojg7',
-    accessKeyId: 'AKIAJO4Y5WK3FS2I3UHA',
-    region: 'us-east-1'
-});
-
-s3 = new AWS.S3();
-
-var gfs = Grid("test", mongo);
-
-const storage = new GridFsStorage({
-  url: config.get("mongoURI"),
-  gfs: gfs,
-  file: (req, file) => {
-    return {
-        filename: file.originalname
-    }
-  }
-});
-var upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'sex-streaming-jerk-n-squirt',
-        key: function (req, file, cb) {
-            console.log(file);
-            cb(null, file.originalname); //use Date.now() for unique file keys
-        }
-    })
-});
+// AWS.config.update({
+//     secretAccessKey: 'r8qpagbd990S45DZUU5wVFTW7K5jG75/NUc0ojg7',
+//     accessKeyId: 'AKIAJO4Y5WK3FS2I3UHA',
+//     region: 'us-east-1'
+// });
+//
+// s3 = new AWS.S3();
+//
+// var gfs = Grid("test", mongo);
+//
+// const storage = new GridFsStorage({
+//   url: config.get("mongoURI"),
+//   gfs: gfs,
+//   file: (req, file) => {
+//     return {
+//         filename: file.originalname
+//     }
+//   }
+// });
+// var upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: 'sex-streaming-jerk-n-squirt',
+//         key: function (req, file, cb) {
+//             console.log(file);
+//             cb(null, file.originalname); //use Date.now() for unique file keys
+//         }
+//     })
+// });
 
 
 mongoose.connect(config.get("mongoURI"),  { useNewUrlParser: true }, { useUnifiedTopology: true }, cors(), (err, db) => {
